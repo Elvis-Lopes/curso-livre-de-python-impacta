a61367ab09c26568cfec0d75c5a4907ec2b6b6c7
elvis <elvislopes1996@hotmail.com>
2021-06-01 12:54:11 -0300

    aula de operadores da linguagem python

---
+++ aula03.py
@@ -0,0 +1,8 @@
+valor1 = int(input("Digite um valor: "))
+valor2 = int(input("Digite um valor: "))
+
+print(f'{valor1 + valor2}')
+print(f'{valor1 - valor2}')
+print(f'{valor1 * valor2}')
+print(f'{valor1 ** valor2}')
+print(f'{valor1 / valor2}')
