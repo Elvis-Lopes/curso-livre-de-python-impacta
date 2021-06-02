70973053aa8757802139c6fbcbc4d3b12c38e956
elvis <elvislopes1996@hotmail.com>
2021-06-02 14:54:15 -0300

    adicionado a função subtrair

--- aula06.py
+++ aula06.py
@@ -1,7 +1,11 @@
 def somar(numero1, numero2):
     print(numero1 + numero2)
 
+def subtrair(numero1, numero2):
+    print(numero1 - numero2)
+
 valor1 = int(input("Digite o primeiro valor: "))
 valor2 = int(input("Digite o segundo valor: "))
 
 somar(valor1, valor2)
+subtrair(valor1, valor2)
