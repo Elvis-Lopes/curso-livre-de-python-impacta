c44138ef3df19b3a2e86682c25ec95399b8e1ee7
elvis <elvislopes1996@hotmail.com>
2021-06-02 15:00:43 -0300

    adicionado a função imprimir parametros

--- aula06.py
+++ aula06.py
@@ -4,8 +4,14 @@ def somar(numero1, numero2):
 def subtrair(numero1, numero2):
     print(numero1 - numero2)
 
+def imprime_parametros(**kwargs):
+    for key, value in kwargs.items():
+        print(key, value)
+
 valor1 = int(input("Digite o primeiro valor: "))
 valor2 = int(input("Digite o segundo valor: "))
 
 somar(valor1, valor2)
 subtrair(valor1, valor2)
+
+imprime_parametros(nome="Ana", sobrenome="Maria")
