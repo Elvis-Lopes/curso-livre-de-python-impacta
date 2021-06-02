6c22bee05daf7adcfe28055e6ba6ec0a13a3c24a
elvis <elvislopes1996@hotmail.com>
2021-06-02 14:51:55 -0300

    aula sobre funções do python. Obs: não há os arquivos das aulas 4 e 5 por ter ja implementado os conceitos apresentados nos arquivos da aula 2 e 3.

---
+++ aula06.py
@@ -0,0 +1,7 @@
+def somar(numero1, numero2):
+    print(numero1 + numero2)
+
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+
+somar(valor1, valor2)
